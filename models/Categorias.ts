41f1d20a4a8ae671ae4ab1e8889e60b2da62b448
MarianoHuitron <47927597+MarianoHuitron@users.noreply.github.com>
2019-11-21 19:54:28 -0700

    Update categorias.ts
    Al tener más de una subcategoría se necesita guardar en un arreglo

--- models/Categorias.ts
+++ models/Categorias.ts
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const CategoriasSchema = new Schema({
     nombre_categoria: { type: String, required: true },
-    nombre_subcategoria: { type: String, required: true }
+    nombre_subcategoria: [{ type: String, required: true }]
 });
 
 export default model('Categorias', CategoriasSchema);
